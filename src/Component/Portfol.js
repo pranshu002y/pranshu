0c6be15105536460fe486499d469fa47eeed6213
Pranshu Yadav <pranshu.yadav@venturepact.com>
2025-06-29 22:01:04 +0530

    projects

--- src/Component/Portfol.js
+++ src/Component/Portfol.js
@@ -1,7 +1,7 @@
-import React from 'react'
-import { useNavigate } from "react-router-dom";
+import React from "react";
+// import { useNavigate } from "react-router-dom";
 const Portfol = () => {
-  const navigate = useNavigate();
+  
   return (
     <div>
       <div className="title">
@@ -9,27 +9,86 @@ const Portfol = () => {
         <div className="title-front">PROJECTS</div>
       </div>
 
-     
       <div className="bigcontainer">
         <div className="insideof">
-       <a href="https://lms-1.netlify.app/accounts/login"><img src="https://raw.githubusercontent.com/Akg-0007/lMS/main/images/Dashboard.png" alt="lms"/></a>
-       <a href="https://scout-verse.vercel.app/"><img src="https://res.cloudinary.com/dpiatasuq/image/upload/v1699117685/Screenshot_1323_ume1ow.png" alt="" /></a>
-       <a href="https://listed-hazel.vercel.app/"><img src="https://res.cloudinary.com/dpiatasuq/image/upload/v1699117996/Screenshot_1327_agwdlt.png" alt="" /></a>
+          <a href="https://lms-1.netlify.app/accounts/login">
+            <img
+              src="https://raw.githubusercontent.com/Akg-0007/lMS/main/images/Dashboard.png"
+              alt="lms"
+            />
+          </a>
+          <a href="https://scout-verse.vercel.app/">
+            <img
+              src="https://res.cloudinary.com/dpiatasuq/image/upload/v1699117685/Screenshot_1323_ume1ow.png"
+              alt=""
+            />
+          </a>
+          <a href="https://listed-hazel.vercel.app/">
+            <img
+              src="https://res.cloudinary.com/dpiatasuq/image/upload/v1699117996/Screenshot_1327_agwdlt.png"
+              alt=""
+            />
+          </a>
         </div>
         <div className="insideof">
-        <a href="https://lms-1.netlify.app/accounts/login"> <img src="https://res-conhttps://codeverse-seven.vercel.app/sole.cloudinary.com/dpiatasuq/thumbnails/v1/image/upload/v1699117685/U2NyZWVuc2hvdF8xMzI0X2ZmMGhxcw==/grid_landscape" alt="" /></a>
-        <a href="https://porsche911.vercel.app/"> <img src="https://res.cloudinary.com/dpiatasuq/image/upload/v1699117826/Screenshot_1325_vo2pak.png" alt="" /></a>
-        <a href="https://nemesis-flame.vercel.app/"> <img src="https://res.cloudinary.com/dpiatasuq/image/upload/v1717567912/Screenshot_Capture_-_2024-06-05_-_11-41-43_ly2rw3.png
-        " alt="" /></a>
+          <a href="https://lms-1.netlify.app/accounts/login">
+            {" "}
+            <img
+              src="https://res-conhttps://codeverse-seven.vercel.app/sole.cloudinary.com/dpiatasuq/thumbnails/v1/image/upload/v1699117685/U2NyZWVuc2hvdF8xMzI0X2ZmMGhxcw==/grid_landscape"
+              alt=""
+            />
+          </a>
+          <a href="https://porsche911.vercel.app/">
+            {" "}
+            <img
+              src="https://res.cloudinary.com/dpiatasuq/image/upload/v1699117826/Screenshot_1325_vo2pak.png"
+              alt=""
+            />
+          </a>
+          <a href="https://nemesis-flame.vercel.app/">
+            {" "}
+            <img
+              src="https://res.cloudinary.com/dpiatasuq/image/upload/v1717567912/Screenshot_Capture_-_2024-06-05_-_11-41-43_ly2rw3.png
+        "
+              alt=""
+            />
+          </a>
+
+          <a href="https://inferno-theta.vercel.app/">
+            {" "}
+            <img
+              src="https://res.cloudinary.com/dpiatasuq/image/upload/v1751214587/Screenshot_Capture_-_2025-06-29_-_21-59-05_g0pl22.png
+        "
+              alt=""
+            />
+          </a>
         </div>
         <div className="insideof">
-        <a href="https://thence-virid.vercel.app/"> <img src="https://res.cloudinary.com/dpiatasuq/image/upload/v1717567796/Screenshot_Capture_-_2024-06-05_-_11-39-31_ye4m3r.png" alt="" /></a>
-        <a href="https://introcom.vercel.app/intro"> <img src="https://res.cloudinary.com/dpiatasuq/image/upload/v1699117933/Screenshot_1326_i76afs.png" alt="" /></a>
-        <a href="https://pranshu002.vercel.app/"> <img src="https://res.cloudinary.com/dpiatasuq/image/upload/v1699118000/Screenshot_1328_gakybz.png" alt="" /></a>
+          <a href="https://thence-virid.vercel.app/">
+            {" "}
+            <img
+              src="https://res.cloudinary.com/dpiatasuq/image/upload/v1717567796/Screenshot_Capture_-_2024-06-05_-_11-39-31_ye4m3r.png"
+              alt=""
+            />
+          </a>
+          <a href="https://introcom.vercel.app/intro">
+            {" "}
+            <img
+              src="https://res.cloudinary.com/dpiatasuq/image/upload/v1699117933/Screenshot_1326_i76afs.png"
+              alt=""
+            />
+          </a>
+          <a href="https://pranshu002.vercel.app/">
+            {" "}
+            <img
+              src="https://res.cloudinary.com/dpiatasuq/image/upload/v1699118000/Screenshot_1328_gakybz.png"
+              alt=""
+            />
+          </a>
         </div>
       </div>
     </div>
   );
-}
+};
 
-export default Portfol
+export default Portfol;
